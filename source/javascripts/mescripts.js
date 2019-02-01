c451b307af6028124370e00bc1a11e3cb5bcd568
serge cleoron <sc21417@gmail.com>
2019-02-01 16:57:50 +0100

    modif illustr

---
+++ source/javascripts/mescripts.js
@@ -0,0 +1,4 @@
+$(window).height();   // returns height of browser viewport
+$(document).height(); // returns height of HTML document (same as pageHeight in screenshot)
+$(window).width();   // returns width of browser viewport
+$(document).width(); // returns width of HTML document (same as pageWidth in screenshot)
