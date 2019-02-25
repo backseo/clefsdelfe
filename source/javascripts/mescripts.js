7b503842440a9477c8eea65ab8107eab1fcbf161
serge cleoron <sc21417@gmail.com>
2019-02-25 11:26:34 +0100

    some java on img

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -1,4 +1,13 @@
-$(window).height();   // returns height of browser viewport
-$(document).height(); // returns height of HTML document (same as pageHeight in screenshot)
-$(window).width();   // returns width of browser viewport
-$(document).width(); // returns width of HTML document (same as pageWidth in screenshot)
+$(document).ready(function() {
+
+  $(".imgover").hover(
+    function(){ $(this).addClass('img-m-over') },
+    function(){ $(this).removeClass('img-m-over') }
+ )
+
+   $(".img").hover(
+    function(){ $(this).addClass('img-m-over') },
+    function(){ $(this).removeClass('img-m-over') }
+ )
+
+});
