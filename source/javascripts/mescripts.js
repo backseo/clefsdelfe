10b0af089906f82e58706381e9750516dab33150
serge cleoron <sc21417@gmail.com>
2019-04-04 14:13:39 +0200

    javascripts button hover

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -8,6 +8,11 @@ $(document).ready(function() {
    $(".img").hover(
     function(){ $(this).addClass('img-m-over') },
     function(){ $(this).removeClass('img-m-over') }
- )
+ );
+
+   let cible = $('.a-wrapper-titles ul li');
+      cible.on('click', function(){
+      $(this).next('.a-container').toggleClass('hider');
+    })
 
 });
