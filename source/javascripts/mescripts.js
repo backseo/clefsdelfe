554621ec610866d8e63a9a1adb02214c1125f60c
serge cleoron <sc21417@gmail.com>
2019-05-06 19:13:23 +0200

    middleman new

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -1,19 +1,8 @@
 $(document).ready(function() {
 
-  $(".imgover").hover(
-    function(){ $(this).addClass('img-m-over') },
-    function(){ $(this).removeClass('img-m-over') }
- )
-
-   $(".img").hover(
-    function(){ $(this).addClass('img-m-over') },
-    function(){ $(this).removeClass('img-m-over') }
- );
-
-   var cible = $('.a-wrapper-titles ul li');
-      cible.on('click', function(){
-      $(this).next('.a-container').toggleClass('hider');
-      console.log('toto');
-    });
+  $("#welcome").hover(
+    function(){ $(".banner-content h2").addClass('reveal') },
+    function(){ $(".banner-content h2").removeClass('reveal') }
+  )
 
 });
