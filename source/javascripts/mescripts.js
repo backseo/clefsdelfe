ccc1de78a95bc60a85df8e21f70c4678f0ef207a
serge cleoron <sc21417@gmail.com>
2019-05-09 18:50:22 +0200

    homepage with navbar banner slider partenaires footer

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-  $(".navbar-lrhg-item").hover(
-    function(){ $(".banner-content h2").addClass('reveal') },
-    function(){ $(".banner-content h2").removeClass('reveal') }
-  )
+  // $(".navbar-lrhg-item").hover(
+  //   function(){ $(".banner-content h2").addClass('reveal') },
+  //   function(){ $(".banner-content h2").removeClass('reveal') }
+  // )
 
 });
