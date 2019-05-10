3c0435c57e3c7dbe5c8b5425704bfb231fba8650
serge cleoron <sc21417@gmail.com>
2019-05-10 17:19:25 +0200

    fix banner-text on resize

--- source/javascripts/navshrink.js
+++ source/javascripts/navshrink.js
@@ -2,11 +2,11 @@
   $(window).scroll(function() {
     console.log('tata');
     if ($(document).scrollTop() > 90) {
-      $('.navbar-clefdelf').addClass('shrink');
-      $('.navbar-clefdelf-brand img').css("height", 60);
+      $('.navbar-clefdelf').css("height", 50);
+      $('.navbar-clefdelf-brand img').css("height",48);
     }
     else {
-      $('.navbar-clefdelf').removeClass('shrink');
+      $('.navbar-clefdelf').css("height", 130);
       $('.navbar-clefdelf-brand img').css("height", 100);
     }
   });
