b5b967d84e091661f422ed1544b5ce7a69c0888b
serge cleoron <sc21417@gmail.com>
2019-05-13 18:22:32 +0200

    navbar shrink icons

--- source/javascripts/navshrink.js
+++ source/javascripts/navshrink.js
@@ -1,13 +1,15 @@
 
   $(window).scroll(function() {
-    console.log('tata');
-    if ($(document).scrollTop() > 90) {
+    if ($(document).scrollTop() > 50) {
       $('.navbar-clefdelf').css("height", 50);
-      $('.navbar-clefdelf-brand img').css("height",48);
+      $('.navbar-clefdelf-brand img').css("height", 48);
+      $('.dropdown-toggle').css("font-size", 20);
     }
     else {
       $('.navbar-clefdelf').css("height", 130);
       $('.navbar-clefdelf-brand img').css("height", 100);
+      $('.dropdown-toggle').css("font-size", 30);
     }
   });
 
+height
