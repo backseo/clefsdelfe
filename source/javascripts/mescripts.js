d9b89cc9491a9cbf19fb94b4e279bc8405b9c421
serge cleoron <sc21417@gmail.com>
2019-05-17 06:14:11 +0200

    seconde revue

--- source/javascripts/mescripts.js
+++ source/javascripts/mescripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 
-  // $(".navbar-lrhg-item").hover(
-  //   function(){ $(".banner-content h2").addClass('reveal') },
-  //   function(){ $(".banner-content h2").removeClass('reveal') }
-  // )
+   /*Smooth the animation to get the anchor tag links*/
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+    });
+  });
 });
+
+
